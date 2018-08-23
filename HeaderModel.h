322a7b38b8868eff1b179c1183402bdf7a82139e
Tomous_Xu <xudacheng_tomous@163.com>
2018-08-23 13:36:31 +0800

    Add files via upload
    修改

---
+++ HeaderModel.h
@@ -0,0 +1,15 @@
+//
+//  HeaderModel.h
+//  secondList
+//
+//  Created by 大橙子 on 2018/6/19.
+//  Copyright © 2018年 中都格罗唯视. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CellModel.h"
+@interface HeaderModel : NSObject
+@property (nonatomic,copy) NSString *title;
+@property (nonatomic,strong) NSArray *cellData;
+
+@end
